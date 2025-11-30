4cdc6d665b0c68acb18a5094d71724ed6b9d8ed4
Prabhkirat Kaur <164630562+simi-835@users.noreply.github.com>
2025-11-30 15:47:04 +0530

    Name:Prabhkirat Kaur ; 1024240024

---
+++ A7 Q1 (b).cpp
@@ -0,0 +1,31 @@
+// Question 7 (b) Insertion Sort :
+//Name:Prabhkirat Kaur ; 1024240024 
+#include <iostream>
+using namespace std;
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    insertionSort(arr, n);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
---
+++ A7 Q1(c).cpp
@@ -0,0 +1,34 @@
+// Question 7 (c) Bubble Sort:
+//Name:Prabhkirat Kaur ; 1024240024 
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    bubbleSort(arr, n);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
